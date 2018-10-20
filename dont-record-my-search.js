c2a2f5d7639cf47f2943339b30bb66f1861de9a7
Shivani Arun <shivaniarun2515@gmail.com>
2018-10-20 15:40:34 +0530

    Fixed #2 Extension triggers every minute now

--- dont-record-my-search.js
+++ dont-record-my-search.js
@@ -7,9 +7,9 @@ deleteMySearch = () => {
   });
 }
 
-// Triggers every 5 minutes
+// Triggers every minute
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.alarms.create('delete-my-search', {when: 0, periodInMinutes: 5});
+  chrome.alarms.create('delete-my-search', {when: 0, periodInMinutes: 1});
 });
 chrome.alarms.onAlarm.addListener((alarm) => {
   if (alarm.name === 'delete-my-search')
